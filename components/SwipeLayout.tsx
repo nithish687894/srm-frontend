56157a082747512ccb7dc109e96c9be62e845268
Nithishkumar S <nithish687894@gmail.com>
2026-09-24 15:05:46 +0530

    fix: restore native mobile scrolling

--- components/SwipeLayout.tsx
+++ components/SwipeLayout.tsx
@@ -371,10 +371,6 @@ export default function SwipeLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <div
-        onTouchStart={onTouchStart}
-        onTouchMove={onTouchMove}
-        onTouchEnd={onTouchEndHandler}
-        onTouchCancel={onTouchEndHandler}
         className={`swipe-wrapper ${!hideSidebar ? 'layout-with-sidebar' : ''}`}
         style={{ overflowX: "hidden", position: "relative", minHeight: "100dvh", width: "100%", maxWidth: "100%", minWidth: 0 }}
       >
